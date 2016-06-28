56581a5f56aa80ea9c756d5eaa47bed12a09bb91
Molly McGlone and Rony Nasr <rony@>
2016-06-28 10:31:59 -0700

    add specs to translate  hundreds, thousands, and millions

---
+++ lib/numbers_to_words.rb
@@ -0,0 +1,39 @@
+require ("pry")
+
+class Fixnum
+  define_method(:translate) do
+    numbers = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen", 20 => "twenty", 30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty", 70 => "seventy", 80 => "eighty", 90 => "ninety"}
+    word = ""
+
+    if !numbers.include?(self)
+      inputNumber = self
+      if (inputNumber./(1000000)).>(0)
+        word.concat(numbers.fetch((inputNumber./(1000000)).floor()))
+        word.concat(" million ")
+        inputNumber = inputNumber.%(1000000)
+      end
+
+      if (inputNumber./(1000)).>(0)
+        word.concat(numbers.fetch((inputNumber./(1000)).floor()))
+        word.concat(" thousand ")
+        inputNumber = inputNumber.%(1000)
+      end
+
+      if (inputNumber./(100)).>(0)
+        word.concat(numbers.fetch((inputNumber./(100)).floor()))
+        word.concat(" hundred ")
+        inputNumber = inputNumber.%(100)
+      end
+
+      if (inputNumber./(10)).>(0)
+        word.concat(numbers.fetch((inputNumber./10).floor().*10))
+        word.concat(" ")
+        inputNumber = inputNumber.%(10)
+      end
+      word.concat(numbers.fetch(inputNumber))
+    else
+      word.concat(numbers.fetch(self))
+    end
+    word
+    end
+end
---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,23 @@
+require ("rspec")
+require ("numbers_to_words")
+
+describe ("Fixnum#numbers_to_words") do
+  it ("returns a word for one number") do
+    expect(19.translate()).to(eq("nineteen"))
+  end
+  it ("returns a two word for compound numbers") do
+    expect(23.translate()).to(eq("twenty three"))
+  end
+  it ("returns a hundreds, tens and units") do
+    expect(235.translate()).to(eq("two hundred thirty five"))
+  end
+
+  it ("returns thousands, hundreds, tens and units") do
+    expect(10235.translate()).to(eq("ten thousand two hundred thirty five"))
+  end
+
+  it ("returns millions, thousands, hundreds, tens and units") do
+    expect(10010235.translate()).to(eq("ten million ten thousand two hundred thirty five"))
+  end
+
+end
